c9928c472904d3f613a049d517b6b684aac44b03
Justin <JustinE@tlgcohort.com>
2024-06-17 14:21:00 -0500

    added PetFactory and its test class

---
+++ src/com/pawsandwhiskers/PetFactory.java
@@ -0,0 +1,11 @@
+package com.pawsandwhiskers;
+
+class PetFactory {
+    private PetFactory() {     //private ctor to prevent outside instantiation
+    }
+
+    //need to have the type of pet selected, Animal enum (either Dog or Cat) determine which we create.
+    //also need a null check to throw an error.
+
+
+}
