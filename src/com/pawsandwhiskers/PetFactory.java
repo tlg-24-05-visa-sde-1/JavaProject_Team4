0e82ff042244c8fd7754a14e98f59556335849a8
Justin <JustinE@tlgcohort.com>
2024-06-17 15:03:26 -0500

    integrated main into branch to have updated code

--- src/com/pawsandwhiskers/PetFactory.java
+++ src/com/pawsandwhiskers/PetFactory.java
@@ -3,8 +3,6 @@ package com.pawsandwhiskers;
 public class PetFactory {
     private PetFactory() {     //private ctor to prevent outside instantiation
     }
-    //do we need an exception here since we're dealing with Enums?  Maybe for String name?
-    //may need to convert from String to Enum?
     public static Pet createPet(Animal animal, String name){
         Pet pet = null;
          if (animal.equals(Animal.DOG)){
